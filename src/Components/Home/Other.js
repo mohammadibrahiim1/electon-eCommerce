86fed93d87a7358ae9f37170d5ced483d0a6e8f4
mohammadibrahiim <mohammadibrahiim1@gmail.com>
2023-01-24 22:12:50 +0600

    brand

--- src/Components/Home/Other.js
+++ src/Components/Home/Other.js
@@ -4,10 +4,9 @@ const Other = () => {
   return (
     <div>
       <section className="other-section container">
-     
-          {/* <a href="https://imgbb.com/"><img src="https://i.ibb.co/Jq0bX8y/11-1-1.png" alt="11-1-1" border="0"></a> */}
-       <div className="card-container">
-       <div
+        {/* <a href="https://imgbb.com/"><img src="https://i.ibb.co/Jq0bX8y/11-1-1.png" alt="11-1-1" border="0"></a> */}
+        <div className="card-container">
+          <div
             class="card mb-3"
             style={{ "max-width": "803px", height: "408px" }}
           >
@@ -39,24 +38,24 @@ const Other = () => {
           </div>
 
           <div className="ms-5 ps-5">
-          <div
-            class="card mb-3 ms-4"
-            style={{ "max-width": "493px", height: "196px" }}
-          >
-            <div class="row g-0">
-              <div class="col-md-4">
-                <img
-                  src="https://i.ibb.co/Jq0bX8y/11-1-1.png"
-                  class="img-fluid rounded-start "
-                  alt="..."
-                />
-              </div>
-              <div class="col-md-8">
-                <div class="card-body mt-4">
-                  <h5 class="card-title ">JBL bar 2.1 deep bass</h5>
-                  <p class="card-text">$180</p>
-                  <p class="card-text">4.7</p>
-                  {/* <p class="card-text">
+            <div
+              class="card mb-3 ms-4"
+              style={{ "max-width": "493px", height: "196px" }}
+            >
+              <div class="row g-0">
+                <div class="col-md-4">
+                  <img
+                    src="https://i.ibb.co/Jq0bX8y/11-1-1.png"
+                    class="img-fluid rounded-start "
+                    alt="..."
+                  />
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body mt-4">
+                    <h5 class="card-title ">JBL bar 2.1 deep bass</h5>
+                    <p class="card-text">$180</p>
+                    <p class="card-text">4.7</p>
+                    {/* <p class="card-text">
                     <small class="btn btn-warning ps-3 pe-3 mt-3">
                       add to cart
                     </small>
@@ -64,31 +63,30 @@ const Other = () => {
                       view
                     </small>
                   </p> */}
-                  {/* <p class="card-text"></p> */}
+                    {/* <p class="card-text"></p> */}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div
-            class="card mb-3 ms-4"
-            style={{ "max-width": "493px", height: "195px" }}
-          >
-            <div class="row g-0">
-              <div class="col-md-4">
-                <img
-                  src="https://i.ibb.co/Jq0bX8y/11-1-1.png" style={{
-
-                  }}
-                  class="img-fluid rounded-start "
-                  alt="..."
-                />
-              </div>
-              <div class="col-md-8">
-                <div class="card-body mt-4">
-                  <h5 class="card-title ">JBL bar 2.1 deep bass</h5>
-                  <p class="card-text">$180</p>
-                  <p class="card-text">4.7</p>
-                  {/* <p class="card-text">
+            <div
+              class="card mb-3 ms-4"
+              style={{ "max-width": "493px", height: "195px" }}
+            >
+              <div class="row g-0">
+                <div class="col-md-4">
+                  <img
+                    src="https://i.ibb.co/Jq0bX8y/11-1-1.png"
+                    style={{}}
+                    class="img-fluid rounded-start "
+                    alt="..."
+                  />
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body mt-4">
+                    <h5 class="card-title ">JBL bar 2.1 deep bass</h5>
+                    <p class="card-text">$180</p>
+                    <p class="card-text">4.7</p>
+                    {/* <p class="card-text">
                     <small class="btn btn-warning ps-3 pe-3 mt-3">
                       add to cart
                     </small>
@@ -96,73 +94,189 @@ const Other = () => {
                       view
                     </small>
                   </p> */}
-                  {/* <p class="card-text"></p> */}
+                    {/* <p class="card-text"></p> */}
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-          </div>
-       </div>
-        
+        </div>
       </section>
 
       <section className="poster-section container">
-
         <div className="poster-card-container">
-            {/* <a href="https://imgbb.com/"><img src="https://i.ibb.co/HtBKqxS/shield-security.png" alt="shield-security" border="0"></a> */}
-            {/* <a href="https://imgbb.com/"><img src="https://i.ibb.co/Qkc65cD/box-tick.png" alt="box-tick" border="0"></a> */}
-
+          {/* <a href="https://imgbb.com/"><img src="https://i.ibb.co/HtBKqxS/shield-security.png" alt="shield-security" border="0"></a> */}
+          {/* <a href="https://imgbb.com/"><img src="https://i.ibb.co/Qkc65cD/box-tick.png" alt="box-tick" border="0"></a> */}
 
           {/* card-1 */}
 
-          <div class="card  mb-3" style={{"max-width": "400px"}}>
-  <div class="row g-0">
-    <div class="col-md-3">
-      <img src="https://i.ibb.co/HtBKqxS/shield-security.png" class="img-fluid mt-3 ms-5" alt="..."/>
-    </div>
-    <div class="col-md-9">
-      <div class="card-body ms-3">
-        <h5 class="card-title">Free delivery</h5>
-        <p class="card-text">on order above $50,00</p>
-      </div>
-    </div>
-  </div>
- 
-</div>
-
-          <div class="card mb-3" style={{"max-width": "400px"}}cd>
-  <div class="row g-0">
-    <div class="col-md-3">
-      <img src="https://i.ibb.co/HtBKqxS/shield-security.png" class="img-fluid mt-3 ms-5" alt="..."/>
-    </div>
-    <div class="col-md-9">
-      <div class="card-body ms-3">
-        <h5 class="card-title">Free delivery</h5>
-        <p class="card-text">on order above $50,00</p>
-      </div>
-    </div>
-  </div>
- 
-</div>
-
-          <div class="card mb-3" style={{"max-width": "400px"}}>
-  <div class="row g-0">
-    <div class="col-md-3">
-      <img src="https://i.ibb.co/HtBKqxS/shield-security.png" class="img-fluid mt-3 ms-5" alt="..."/>
-    </div>
-    <div class="col-md-9">
-      <div class="card-body ms-3">
-        <h5 class="card-title">Free delivery</h5>
-        <p class="card-text">on order above $50,00</p>
-      </div>
-    </div>
-  </div>
- 
-</div>
-
-
+          <div
+            class="card poster-card-bg p-3 mb-3"
+            style={{ "max-width": "400px" }}
+          >
+            <div class="row g-0">
+              <div class="col-md-3">
+                <img
+                  src="https://i.ibb.co/HtBKqxS/shield-security.png"
+                  class="img-fluid mt-3 ms-5"
+                  alt="..."
+                />
+              </div>
+              <div class="col-md-9">
+                <div class="card-body ms-3">
+                  <h5 class="card-title">Free delivery</h5>
+                  <p class="card-text">on order above $50,00</p>
+                </div>
+              </div>
             </div>
-           
+          </div>
+
+          <div
+            class="card poster-card-bg p-3 mb-3"
+            style={{ "max-width": "400px" }}
+            cd
+          >
+            <div class="row g-0">
+              <div class="col-md-3">
+                <img
+                  src="https://i.ibb.co/Qkc65cD/box-tick.png"
+                  class="img-fluid mt-3 ms-5"
+                  alt="..."
+                />
+              </div>
+              <div class="col-md-9">
+                <div class="card-body ms-3">
+                  <h5 class="card-title">Free delivery</h5>
+                  <p class="card-text">on order above $50,00</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div
+            class="card poster-card-bg p-3 mb-3"
+            style={{ "max-width": "400px" }}
+          >
+            <div class="row g-0">
+              <div class="col-md-3">
+                <img
+                  src="https://i.ibb.co/HtBKqxS/shield-security.png"
+                  class="img-fluid mt-3 ms-5"
+                  alt="..."
+                />
+              </div>
+              <div class="col-md-9">
+                <div class="card-body ms-3">
+                  <h5 class="card-title">Free delivery</h5>
+                  <p class="card-text">on order above $50,00</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="container">
+        <div>
+          <h3 className="review-title mb-4">Reviews</h3>
+        </div>
+
+        <div className="review-container">
+          <div class="card shadow mb-3" style={{ "max-width": "400px" }}>
+            <div class="row g-0">
+              <div class="col-md-3">
+                {/* <a href="https://ibb.co/C2vj9vX"><img src="https://i.ibb.co/GTkZMk1/cover.png" alt="cover" border="0"></a> */}
+                <img
+                  src="https://i.ibb.co/GTkZMk1/cover.png"
+                  class="img-fluid mt-3 ms-5"
+                  alt="..."
+                />
+              </div>
+              <div class="col-md-9">
+                <div class="card-body mt-3 ms-5">
+                  <h5 class="card-title">Savannah Nguyen</h5>
+                </div>
+              </div>
+              <p class="card-text review ms-5 p-3 mt-2 mb-3">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos
+                veniam obcaecati eaque soluta impedit{" "}
+              </p>
+            </div>
+          </div>
+          <div class="card shadow  mb-3" style={{ "max-width": "400px" }}>
+            <div class="row g-0">
+              <div class="col-md-3">
+                {/* <a href="https://ibb.co/C2vj9vX"><img src="https://i.ibb.co/GTkZMk1/cover.png" alt="cover" border="0"></a> */}
+                <img
+                  src="https://i.ibb.co/GTkZMk1/cover.png"
+                  class="img-fluid mt-3 ms-5"
+                  alt="..."
+                />
+              </div>
+              <div class="col-md-9">
+                <div class="card-body mt-3 ms-5">
+                  <h5 class="card-title">Savannah Nguyen</h5>
+                </div>
+              </div>
+              <p class="card-text review ms-5 p-3 mt-2 mb-3">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos
+                veniam obcaecati eaque soluta impedit{" "}
+              </p>
+            </div>
+          </div>
+          <div class="card shadow  mb-3" style={{ "max-width": "400px" }}>
+            <div class="row g-0">
+              <div class="col-md-3">
+                {/* <a href="https://ibb.co/C2vj9vX"><img src="https://i.ibb.co/GTkZMk1/cover.png" alt="cover" border="0"></a> */}
+                <img
+                  src="https://i.ibb.co/GTkZMk1/cover.png"
+                  class="img-fluid mt-3 ms-5"
+                  alt="..."
+                />
+              </div>
+              <div class="col-md-9">
+                <div class="card-body mt-3 ms-5">
+                  <h5 class="card-title">Savannah Nguyen</h5>
+                </div>
+              </div>
+              <p class="card-text review ms-5 p-3 mt-2 mb-3">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos
+                veniam obcaecati eaque soluta impedit{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="brand-section container m-auto">
+        {/* <a href="https://ibb.co/GMX2Rqh"><img src="https://i.ibb.co/Sm2nBzL/brand-8.png" alt="brand-8" border="0"></a> */}
+        <div className="brand-container p-3">
+          <img
+            src="https://i.ibb.co/Sm2nBzL/brand-8.png"
+            alt=""
+            style={{ width: "206px", height: "41px" }}
+          />
+          <img
+            src="https://i.ibb.co/Sm2nBzL/brand-8.png"
+            alt=""
+            style={{ width: "206px", height: "41px" }}
+          />
+          <img
+            src="https://i.ibb.co/Sm2nBzL/brand-8.png"
+            alt=""
+            style={{ width: "206px", height: "41px" }}
+          />
+          <img
+            src="https://i.ibb.co/Sm2nBzL/brand-8.png"
+            alt=""
+            style={{ width: "206px", height: "41px" }}
+          />
+          <img
+            src="https://i.ibb.co/Sm2nBzL/brand-8.png"
+            alt=""
+            style={{ width: "206px", height: "41px" }}
+          />
+        </div>
       </section>
     </div>
   );
