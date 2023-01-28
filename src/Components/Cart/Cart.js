42ceae43959cf8ba20f22397fe743f2f31c2a00a
mohammadibrahiim <mohammadibrahiim1@gmail.com>
2023-01-28 19:01:18 +0600

    update

---
+++ src/Components/Cart/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Cart.css'
+
+const Cart = () => {
+    return (
+        <div>
+            see all products 
+        </div>
+    );
+};
+
+export default Cart;
